cab757e0bd97459d7c2cff46ffd2e7372ec96086
Jetii8 <jessicalilian.lopez@icloud.com>
2024-02-12 00:46:02 -0700

    crear una clase de doctor

---
+++ Doctor.java
@@ -0,0 +1,13 @@
+public class Doctor {
+
+    private String especialiad;
+
+    public Doctor(
+        String nombre,
+        String correo,
+        String especialiad
+    ){
+        super(nombre, correo)
+        this.especialiad = especialiad;
+    }
+}
